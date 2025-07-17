3432a92a954e0a336c02837fb1bbe2ca8382ce2e
Az-san <kudo.maiko933@mail.kyutech.jp>
2025-07-17 18:25:08 +0900

    modified requirements.txt docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# --- ここでバージョン番号を指定 ---
-export IMAGE_TAG="1.0.0" 
-# --------------------------------
+# バージョン番号を指定
+export IMAGE_TAG="1.0.0"
 
-echo "Building and running image ros_dev:${IMAGE_TAG}..."
+# 毎回変わる現在時刻（秒）をキャッシュ無効化用の変数に設定
+export CACHE_BUSTER=$(date +%s)
+
+echo "Building and running image ros_dev:${IMAGE_TAG} with cache buster ${CACHE_BUSTER}..."
 
 # Docker Composeを実行
-docker-compose run --rm ros bash
+docker-compose run --build --rm ros bash
